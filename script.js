6c59ab6b706ed2cb8386759b60768a006cd356a9
shubh-codes7 <shubhamcodes7@gmail.com>
2025-04-03 13:54:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let user = document.querySelector("#username")
+let pwd = document.querySelector("#password")
+let checkBox = document.querySelector("#checkbox")
+let btn = document.querySelector("#submit")
+let existingUser = document.querySelector("#existing")
+
+if(localStorage.getItem("username")){
+	existingUser.style.display = "inline"
+}
+
+let v = "shu"
+existingUser.addEventListener("click", ()=>{
+	alert(`Logged in as ${v}`);
+})
+
+btn.addEventListener("click", ()=>{
+	alert("Logged in as ", user.value);
+	if(checkBox.checked){
+		localStorage.setItem("username", user.value)
+		localStorage.setItem("password", pwd.value)
+	}else{
+		localStorage.clear()
+	}
+	
+})
+
+
+
+
